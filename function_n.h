43fd7415a61b497dc54c33bad185115523cc0ec1
Cadmium-CD <512256738@qq.com>
2018-02-24 00:36:09 +0800

    Add code for finding intersection

--- function_n.h
+++ function_n.h
@@ -1,10 +1,11 @@
 #define CH_NUM  8
 #define BUF_LEN 1025
-#define SAMP_FREQ 50000
+#define SAMP_FREQ 39423
 //#define INTERAL_US 1e6/SAMP_FREQ
- #define INTERAL_US 20
+ #define INTERAL_US 22
 #define gccNUM 101
 #define pi 3.1415926
+#define sound 341.0
 SPI spi(p5, p6, p7); // mosi, miso, sclk
 DigitalOut cs(p8);
 InterruptIn button(p9);
@@ -20,12 +21,17 @@ float s;
 int16_t sample1[BUF_LEN];  // store the values read from ADC
 int16_t sample2[BUF_LEN]; 
 int16_t sample3[BUF_LEN]; 
+int16_t sample4[BUF_LEN]; 
 float sample1_s[BUF_LEN];  
 float sample2_s[BUF_LEN]; 
 float sample3_s[BUF_LEN]; 
+float sample4_s[BUF_LEN]; 
+
 int r12 = 0;
 int r13 = 5;
+int r14 = 5;
 int r23 = 10;
+int r24 = 10;
 
 int wp = 0;
 
@@ -72,6 +78,11 @@ void scalling(){
      for (i=0; i<BUF_LEN; i++) {  
         sample1_s[i] = 2*(sample3[i]-mi)/float(ma-mi)-1;
     }   
+     ma = (max(sample4,BUF_LEN));
+     mi = (min(sample4,BUF_LEN));
+     for (i=0; i<BUF_LEN; i++) {  
+        sample4_s[i] = 2*(sample4[i]-mi)/float(ma-mi)-1;
+    }    
 }
 int xcorr(float *x1,float *x2){
     
@@ -113,9 +124,18 @@ int xcorr(float *x1,float *x2){
         return j;
 }
 double cal_angle(){
-     double alpha, angle;
-     alpha = 1 - r13*2/double(r12);
-     angle = atan(alpha)/pi*180;
+     double alpha1, alpha2, beta1,beta2,delay1,delay2,delay3,x,y,angle;
+     delay1 = double(r12)/SAMP_FREQ;
+     delay2 = double(r13)/SAMP_FREQ;
+     delay3 = double(r24)/SAMP_FREQ;
+     alpha1 = 1 - r13*2/double(r12);
+     alpha2 = 1 - r24*2/double(-r12);
+     beta1 = 341*341*2*(delay1-delay2)*delay2;
+     beta2 = 341*341*2*(-delay1-delay3)*delay3;
+     x = (-beta2-beta1)/(alpha1-alpha2);
+     y = x*alpha1 + beta1;
+     angle = atan(y/x)/pi*180;
+     printf("alpha1 = %f beta1 = %f\nalpha2 = %f beta2 = %f\n",alpha1, beta1,alpha2 ,beta2);
      
      if (r12 > 0){
       angle = angle + 180;
