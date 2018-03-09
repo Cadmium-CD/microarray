3485cf03b48eb7e49ba23eb719e65a9f41a5ba80
Cadmium-CD <512256738@qq.com>
2018-03-09 19:49:42 +0800

    Update bf.c

--- Beamforming/bf.c
+++ Beamforming/bf.c
@@ -1,15 +1,16 @@
-#define D 0.25
+#define D 0.07
 #define CH_NUM  4
 #define RES 2
 #define ANG_NUM 360/RES
-#define OUT_LEN 512
+#define OUT_LEN 10
+#define TOL_LEN 1000
 
 
 
 //int ang_tri[ANG_NUM];
 int ang_tri[CH_NUM];
 double mic_pos[36/CH_NUM];
-int results[OUT_LEN]; 
+int results[TOL_LEN]; 
 double dir_es;
 
 
@@ -29,7 +30,7 @@ void initialize(){
 
 
 void beamforming(){
-     for(int i = 0;i < OUT_LEN;i++){
+     for(int i = 0;i < TOL_LEN;i++){
         results[i] = (sample1[OUT_LEN-ang_tri[0]+i]+sample2[OUT_LEN-ang_tri[1]+i]+sample3[OUT_LEN-ang_tri[2]+i]+sample4[OUT_LEN-ang_tri[3]+i])/CH_NUM; 
         }
     
